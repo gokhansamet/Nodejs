ff86a729146f6eb14dfb97704750405ce3bab181
Gökhan Samet Albayrak <gokhanalbayrak43@gmail.com>
2021-02-11 00:16:43 +0300

    Build simple API

--- Introduction To Nodejs/main.js
+++ Introduction To Nodejs/main.js
@@ -1,5 +1,9 @@
+const fs = require("fs");
 const http = require("http");
 const url = require("url");
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const path = req.url;
@@ -8,6 +12,11 @@ const server = http.createServer((req, res) => {
     res.end("Hello from the server");
   } else if (path === "/product") {
     res.end("This is a product page");
+  } else if (path === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
   } else {
     res.writeHead(400, {
       "Content-type": "text/html",
@@ -17,6 +26,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(8000, "localhost", () => {
-  console.log("Listening to request on port 8000 is successful");
+server.listen(3000, "localhost", () => {
+  console.log("Listening to request on port 3000 is successful");
 });
