fa64b7a2ddcdc7070f14ca9d2a6179afffac61f8
Gökhan Samet Albayrak <gokhanalbayrak43@gmail.com>
2021-02-10 17:10:26 +0300

    Create routing

--- Introduction To Nodejs/main.js
+++ Introduction To Nodejs/main.js
@@ -1,7 +1,20 @@
 const http = require("http");
+const url = require("url");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from the server");
+  const path = req.url;
+
+  if (path === "/" || path === "/overview") {
+    res.end("Hello from the server");
+  } else if (path === "/product") {
+    res.end("This is a product page");
+  } else {
+    res.writeHead(400, {
+      "Content-type": "text/html",
+      "my-own-header": "Hello World",
+    });
+    res.end("Page Not Found!");
+  }
 });
 
 server.listen(8000, "localhost", () => {
