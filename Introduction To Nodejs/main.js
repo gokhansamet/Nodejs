55c50c319c7f02cbf0a48195012bc627d78f1170
Gökhan Samet Albayrak <gokhanalbayrak43@gmail.com>
2021-02-11 16:37:02 +0300

    Fill the templates

--- Introduction To Nodejs/main.js
+++ Introduction To Nodejs/main.js
@@ -2,14 +2,50 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
+// We use synchronous way because the code below just executed once.
+// The codes below read the data from html once
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  }
+  return output;
+};
+
 const server = http.createServer((req, res) => {
   const path = req.url;
-
+  // The overview page
   if (path === "/" || path === "/overview") {
-    res.end("Hello from the server");
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
   } else if (path === "/product") {
     res.end("This is a product page");
   } else if (path === "/api") {
