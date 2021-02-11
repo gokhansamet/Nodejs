661031e7869101f34fde035767e618379a9ccf4f
Gökhan Samet Albayrak <gokhanalbayrak43@gmail.com>
2021-02-11 17:44:19 +0300

    Create own modules (export/import)

--- Node Farm/main.js
+++ Node Farm/main.js
@@ -20,20 +20,7 @@ const tempCard = fs.readFileSync(
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  }
-  return output;
-};
+const replaceTemplate = require("./modules/replaceTemplate");
 
 const server = http.createServer((req, res) => {
   const path = req.url;
