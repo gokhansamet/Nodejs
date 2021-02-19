1b9a8f5363f01bfbad6252d80a5e3252aefa6ac2
Gökhan Samet Albayrak <gokhanalbayrak43@gmail.com>
2021-02-19 22:46:56 +0300

    Get data from the file (example GET)

--- Natours Project/app.js
+++ Natours Project/app.js
@@ -1,13 +1,29 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello from the server side', app: 'Natours' });
-});
+// app.get('/', (req, res) => {
+//   res.json({ message: 'Hello from the server side', app: 'Natours' });
+// });
 
-app.post('/', (req, res) => {
-  res.send('You can post to the server');
+// app.post('/', (req, res) => {
+//   res.send('You can post to the server');
+// });
+
+// Reading a data from a file using synchronous way
+// It is okay because it is a top-level-code it executes once after the code starts
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'Success',
+    results: tours.length,
+    data: {
+      tours: tours, // Just writing tours is enough
+    },
+  });
 });
 
 const port = 3000;
