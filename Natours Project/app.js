bc57a5744875a2e5228518d60fce102bbaf16beb
Gökhan Samet Albayrak <gokhanalbayrak43@gmail.com>
2021-02-20 16:25:08 +0300

    Refactoring Routes

--- Natours Project/app.js
+++ Natours Project/app.js
@@ -3,13 +3,6 @@ const fs = require('fs');
 const app = express();
 // For using req.body
 app.use(express.json());
-// app.get('/', (req, res) => {
-//   res.json({ message: 'Hello from the server side', app: 'Natours' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.send('You can post to the server');
-// });
 
 // Reading a data from a file using synchronous way
 // It is okay because it is a top-level-code it executes once after the code starts
@@ -17,7 +10,7 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'Success',
     results: tours.length,
@@ -25,13 +18,9 @@ app.get('/api/v1/tours', (req, res) => {
       tours: tours, // Just writing tours is enough
     },
   });
-});
+};
 
-// To create a varaible which in the URL use (:) without parantheses then write a parameter name
-// Ex. :category
-// To create an optional variable use (?) after the parameter name
-// Ex. :category?
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
   const id = req.params.id * 1; // To convert string to int
   const tour = tours.find((el) => el.id === id);
   if (!tour) {
@@ -46,10 +35,9 @@ app.get('/api/v1/tours/:id', (req, res) => {
       tour,
     },
   });
-});
+};
 
-// Create a tour with POST
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
   // To assign an id, while we use database we don't assign an id manuel because database assigns automically
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
@@ -66,7 +54,13 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
+};
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+// To create a varaible which in the URL use (:) without parantheses then write a parameter name
+// Ex. :category
+// To create an optional variable use (?) after the parameter name
+// Ex. :category?
+app.route('/api/v1/tours/:id').get(getTour);
 
 const port = 3000;
 app.listen(port, () => {
