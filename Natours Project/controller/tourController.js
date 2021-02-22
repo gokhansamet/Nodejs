87c0a0f117ddeaa33476fa2ddcc6ffe3d5634443
Gökhan Samet Albayrak <gokhanalbayrak43@gmail.com>
2021-02-23 00:21:28 +0300

    Add chaining multiple middleware functions (check req body)

--- Natours Project/controller/tourController.js
+++ Natours Project/controller/tourController.js
@@ -5,12 +5,22 @@ const fs = require('fs');
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
+
 exports.checkID = (req, res, next, val) => {
   console.log(`This id : ${val}`);
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'Failed',
       message: 'Invalid ID',
+    });
+  }
+  next();
+};
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'Failed',
+      message: 'Missing name or price',
     });
   }
   next();
